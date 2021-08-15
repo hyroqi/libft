30940890b96e59f2343b5d9c47fa967aff96b414
hyroqi <hiroq.i@outlook.com>
2021-08-15 21:31:53 +0800

    libft.h fix

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -8,7 +8,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
-	finalstr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) * sizeof(char)));
+	finalstr = (char *)malloc(((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char)));
 	if (!finalstr)
 		return (0);
 	while (s1 != '\0')
@@ -24,5 +24,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i++;
 		j++;
 	}
+	finalstr[i] = '\0';
 	return (finalstr);
 }
--- libft.h
+++ libft.h
@@ -3,17 +3,17 @@
 	#include <stdlib.h>
 	#include <unistd.h>
 
-	int	ft_atoi(const char *nptr);
+	int		ft_atoi(const char *nptr);
 	void	ft_bzero(void *s, size_t n);
-	void	*calloc(size_t nmemb, size_t size);
-	int	ft_isalnum(int c);
-	int	ft_isalpha(int c);
-	int	ft_isascii(int c);
-	int	ft_isdigit(int c);
-	int	ft_isprint(int c);
+	void	*ft_calloc(size_t nmemb, size_t size);
+	int		ft_isalnum(int c);
+	int		ft_isalpha(int c);
+	int		ft_isascii(int c);
+	int		ft_isdigit(int c);
+	int		ft_isprint(int c);
 	char	*ft_itoa(int n);
 	void	*ft_memchr(const void *str, int c, size_t n);
-	int	ft_memcmp(const void*s1, const void *s2, size_t n);
+	int		ft_memcmp(const void*s1, const void *s2, size_t n);
 	void	*ft_memcpy(void *dest, const void *src, size_t n);
 	void	*ft_memmove(void *dest, const void *src, size_t n);
 	void	*ft_memset(void *s, int c, size_t n);
@@ -27,9 +27,9 @@
 	char	*ft_strjoin(char const *s1, char const *s2);
 	size_t	ft_strlcat(char *dst, const char *src, size_t size);
 	size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-	int	ft_strlen(const char *c);
+	int		ft_strlen(const char *c);
 	char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-	int	strncmp(const char *str1, const char *str2, size_t n);
+	int		ft_strncmp(const char *str1, const char *str2, size_t n);
 	char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
 	char	*ft_strrchr(const char *s, int c);
 	void	ft_strteri(char *s, void (*f)(unsigned int, char*));
