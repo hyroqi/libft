eded121cb1a72a52298ccaa9a602c3f1adff0a64
hyroqi <hiroq.i@outlook.com>
2021-08-17 07:02:18 +0800

    fuckoffmemmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -5,8 +5,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*ds;
 	unsigned char	*sc;
 
-	if (!dest || !src || !n)
+	if (!dest || !src)
 		return (NULL);
+	if (n == 0)
+		return (dest);
 	ds = ((unsigned char *)dest);
 	sc = ((unsigned char *)src);
 	if (sc < ds)
