c3f41e4d2a0c4f6c17e8a0d186cbc4b4fac3669c
hyroqi <hiroq.i@outlook.com>
2021-08-17 02:59:23 +0800

    kekW

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -2,14 +2,10 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
-	int		j;
 	int		combstrlen;
 	char	*buffer;
 	char	*finalstr;
 
-	i = 0;
-	j = 0;
 	combstrlen = (ft_strlen(s1) + ft_strlen(s2));
 	if (s1 && s2)
 	{
@@ -17,10 +13,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		if (buffer)
 		{
 			finalstr = buffer;
-			while (s1[i] != '\0')
-				buffer[i] = s1[i++];
-			while (s2[j] != '\0')
-				buffer[i++] = s2[j++];
+			while (*s1 != '\0')
+				*buffer++ = *s1++;
+			while (*s2 != '\0')
+				*buffer = *s2++;
 			return (finalstr);
 		}
 		return (NULL);
