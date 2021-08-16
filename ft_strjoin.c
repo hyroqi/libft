9748f410c9880df1f505399b475bc3f8566e47cb
hyroqi <hiroq.i@outlook.com>
2021-08-16 23:29:26 +0800

    strjoin new

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -4,26 +4,26 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	int		i;
 	int		j;
+	int		combstrlen;
+	char	*buffer;
 	char	*finalstr;
 
 	i = 0;
 	j = 0;
-	finalstr = (char *)malloc(((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char)));
-	if (!finalstr)
-		return (0);
-	while (s1 != '\0')
+	combstrlen = (ft_strlen(s1) + ft_strlen(s2));
+	if (s1 && s2)
 	{
-		finalstr[i] = s1[j];
-		i++;
-		j++;
+		buffer = (char *)malloc(combstrlen * sizeof(char));
+		if (buffer)
+		{
+			finalstr = buffer;
+			while (s1[i] != '\0')
+				buffer[i] = s1[i++];
+			while (s2[j] != '\0')
+				buffer[i++] = s2[j++];
+			return (finalstr);
+		}
+		return (NULL);
 	}
-	j = 0;
-	while (s2 != '\0')
-	{
-		finalstr[i] = s2[j];
-		i++;
-		j++;
-	}
-	finalstr[i] = '\0';
-	return (finalstr);
+	return (NULL);
 }
