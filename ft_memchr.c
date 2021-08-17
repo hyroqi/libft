b63783e131856ee559f006dfa899d038eb0e1954
hyroqi <hiroq.i@outlook.com>
2021-08-17 22:08:42 +0800

    tester

--- ft_memchr.c
+++ ft_memchr.c
@@ -4,8 +4,11 @@ void	*ft_memchr(const void *str, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*sr;
+	unsigned int	cast;
+
 
 	i = 0;
+	cast = (unsigned int)c;
 	sr = ((unsigned char *)str);
 	while (i < n)
 	{
