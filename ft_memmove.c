ea3d014f6e16518eeaabe92b8a6c5d8ba814645b
hyroqi <hiroq.i@outlook.com>
2021-08-17 21:55:04 +0800

    ;mitake

--- ft_memmove.c
+++ ft_memmove.c
@@ -5,7 +5,7 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*ds;
 	unsigned char	*sc;
 
-	if ((!dest && !src) || n == 0);
+	if ((!dest && !src) || n == 0)
 		return (dest);
 	ds = ((unsigned char *)dest);
 	sc = ((unsigned char *)src);
