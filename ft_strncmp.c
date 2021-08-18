b651b03ea76c3ec9a2ea04f000be22dc65ec0f2d
hyroqi <hiroq.i@outlook.com>
2021-08-18 15:32:04 +0800

    strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -9,10 +9,13 @@ int	ft_strncmp(const char *str1, const char *str2, size_t n)
 	i = 0;
 	s1 = ((unsigned char *)str1);
 	s2 = ((unsigned char *)str2);
-	while (s1[i] == s2[i] && i < n && s1[i] != '\0')
-		i++;
-	if (s1[i] == s2[i])
+	if (!str1 || !str2)
 		return (0);
-	else
-		return ((int)s1[i] - (int)s2[i]);
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
