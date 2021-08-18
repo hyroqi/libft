3c373e9de3e094b20d8114c3bad14d3c1f45cd3d
hyroqi <hiroq.i@outlook.com>
2021-08-18 14:15:58 +0800

    memchr cast

--- ft_memchr.c
+++ ft_memchr.c
@@ -4,11 +4,11 @@ void	*ft_memchr(const void *str, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*sr;
-	unsigned int	cast;
+	unsigned int	*cast;
 
 
 	i = 0;
-	cast = (unsigned int)c;
+	cast = (unsigned char *)c;
 	sr = ((unsigned char *)str);
 	while (i < n)
 	{
