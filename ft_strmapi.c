85153bf6076cf9d5921288e75a5c61f411ca2a5f
hyroqi <hiroq.i@outlook.com>
2021-08-18 15:44:28 +0800

    123123123123123123123

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -4,11 +4,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char				*res;
 	unsigned int		i;
+	int					strlength;
 
 	i = 0;
 	if (!f || !s)
 		return (0);
-	res = (char *)malloc(ft_strlen(s) * sizeof(char));
+	strlength = (ft_strlen(s) + 1);
+	res = (char *)malloc(strlength * sizeof(char));
 	if (!res)
 		return (res);
 	while (s[i] != '\0')
@@ -16,5 +18,6 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 		res[i] = f(i, s[i]);
 		i++;
 	}
+	res[i] = '\0';
 	return (res);
 }
