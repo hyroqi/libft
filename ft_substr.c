6fc0fd9c5319e3c3cc3cb834be63b63a19ac503f
hyroqi <hiroq.i@outlook.com>
2021-08-18 15:52:26 +0800

    test

--- ft_substr.c
+++ ft_substr.c
@@ -7,7 +7,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*substr;
 
 	i = 0;
+	if (!s || !start || !len)
+		return (0);
 	strt = ((size_t)start);
+	if ((size_t)ft_strlen(s) < strt)
+		return (ft_strdup(""));
 	substr = (char *)malloc((len + 1) * sizeof(char));
 	if (!substr)
 		return (0);
