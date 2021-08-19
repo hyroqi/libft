28721a486582d2367e30776fb8b928efa28ec26b
hyroqi <hiroq.i@outlook.com>
2021-08-19 18:51:19 +0800

    uncasted to unsigned int

--- ft_substr.c
+++ ft_substr.c
@@ -2,27 +2,24 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	strt;
-	char	*substr;
+	unsigned int	i;
+	char			*substr;
 
 	i = 0;
 	if (!s)
 		return (0);
-	strt = ((size_t)start);
-	if ((size_t)ft_strlen(s) < strt)
+	if ((size_t)ft_strlen(s) < start)
 		return (ft_strdup(""));
 	if ((size_t)ft_strlen(s) < len)
-		substr = (char *)malloc((ft_strlen(s) - strt + 1) * sizeof(char));
+		substr = (char *)malloc((ft_strlen(s) - start + 1) * sizeof(char));
 	else
 		substr = (char *)malloc((len + 1) * sizeof(char));
 	if (!substr)
 		return (0);
 	while (i < len)
 	{
-		substr[i] = s[strt + i];
+		substr[i] = s[start + i];
 		i++;
-		strt++;
 	}
 	substr[i] = '\0';
 	return (substr);
