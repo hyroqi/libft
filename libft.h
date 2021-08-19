332881425dfbd29f599cd5d2d836ce4120f83e68
hyroqi <hiroq.i@outlook.com>
2021-08-19 19:03:07 +0800

    yourmom

--- libft.h
+++ libft.h
@@ -27,7 +27,7 @@ char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-int		ft_strlen(const char *c);
+size_t		ft_strlen(const char *c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *str1, const char *str2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t n);
