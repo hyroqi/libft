40f27f987d671d22003d6170841e07c0d4c713bc
hyroqi <hiroq.i@outlook.com>
2021-08-19 19:31:37 +0800

    patsplit

--- ft_split.c
+++ ft_split.c
@@ -1,77 +1,49 @@
 #include "libft.h"
 
-static int	ft_wordcount(char *s, char c)
+static size_t	ft_count(char const *str, char c)
 {
-	int	i;
-	int	isword;
-	int	wordcount;
+	size_t	i;
+	size_t	count;
 
 	i = 0;
-	isword = 0;
-	wordcount = 0;
-	while (s[i] != '\0')
+	count = 0;
+	while (*(str + i) != '\0')
 	{
-		if (isword == 0 && s[i] != c)
-		{
-			isword = 1;
-			wordcount++;
-		}
-		else if (isword == 0 && s[i] == c)
-			isword = 0;
-		i++;
+		while (*(str + i) == c)
+			i++;
+		if (*(str + i) != c && *(str + i) != '\0')
+			count += 1;
+		while (*(str + i) != c && *(str + i) != '\0')
+			i++;
 	}
-	return (wordcount);
+	return (count);
 }
 
-static int	ft_wordstrlen(char *s, char c)
+char	**ft_split(char const *s, char c)
 {
-	int	len;
-	int	i;
+	char	**str;
+	size_t	i;
+	size_t	j;
+	size_t	k;
 
-	len = 0;
-	i = 0;
-	while (s[i] != c && s[i] != '\0')
-	{
-		len++;
-		i++;
-	}
-	return (len);
-}
-
-static void	*ft_free(char **res, int n)
-{
-	while (n > 0)
-	{
-		free(res[n]);
-		n--;
-	}
-	free(res);
-	return (0);
-}
-
-char	**ft_split(char const*s, char c)
-{
-	int		wordcount;
-	char	**res;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
 	if (!s)
 		return (0);
-	wordcount = ft_wordcount((char *)s, c);
-	res = (char **)malloc((wordcount + 1) * sizeof(char *));
-	while (wordcount--)
+	i = 0;
+	k = 0;
+	str = malloc(sizeof(s) * (ft_count(s, c) + 1));
+	if (!str)
+		return (0);
+	while (*(s + i) != '\0' && k < ft_count(s, c))
 	{
-		while (s[i] == c && s[i] != '\0')
+		j = 0;
+		while (*(s + i) == c)
 			i++;
-		res[j] = ft_substr((char *)s, 0, ft_wordstrlen((char *)s, c));
-		if (!res[j])
-			return (ft_free(res, j));
-		s = s + ft_wordcount((char *)s, c);
-		j++;
+		while (*(s + i + j) != c && *(s + i + j) != '\0')
+			j++;
+		str[k] = malloc(sizeof(*s) * (j + 1));
+		ft_strlcpy(str[k++], s + i, j + 1);
+		i = i + j + 1;
 	}
-	res[j] = NULL;
-	return (res);
+	str[k] = 0;
+	return (str);
 }
