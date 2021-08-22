ceb28770052a531d12df0869b812605d9c4da524
hyroqi <hiroq.i@outlook.com>
2021-08-22 21:49:22 +0800

    makefile and libfth bonus fix

--- libft.h
+++ libft.h
@@ -46,4 +46,10 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	ft_tolower(int c);
 char	ft_toupper(int c);	
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+
 #endif
