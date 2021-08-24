e653355cad1b639833cc176b0ededf934cf2d5f0
hyroqi <hiroq.i@outlook.com>
2021-08-25 03:30:26 +0800

    peer

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,6 +6,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*ds;
 	const unsigned char	*sr;
 
+	if (n == 0)
+		return (NULL);
 	i = 0;
 	ds = (unsigned char *)dest;
 	sr = (unsigned char *)src;
