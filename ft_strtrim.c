271ed45c14a7a9a34a6dbcb3b80285f15ab3a99c
hyroqi <hiroq.i@outlook.com>
2021-08-25 02:47:28 +0800

    test

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -23,6 +23,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (start > end)
 		return (ft_strdup(""));
 	res = (char *)malloc((end - start + 1) * sizeof(char *));
-	ft_strlcpy(res, s1 + start, end - start + 2);
+	ft_strlcpy(res, s1 + start, end - start + 1);
 	return (res);
 }
