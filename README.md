74125ff9e84343bf94ba1e2d75ef4a75055784e8
hyroqi <hiroq.i@outlook.com>
2021-08-25 02:04:44 +0800

    strtrim test

--- README.md
+++
@@ -1,2 +0,0 @@
-# libft
-Literally just a plce where i store my libft stuff incase i get fucked yikes
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,15 +14,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 	end = (ft_strlen(s1) - 1);
 	while (s1[start] != '\0' && ft_strchr(set, s1[start]))
 		start++;
-	while (s1[end] != '\0' && ft_strrchr(set, s1[end]))
+	while (s1[end--] != '\0' && ft_strrchr(set, s1[end]))
 	{
 		if ((end - 1) < 1)
 			break ;
-		end--;
 	}
 	if (start > end)
 		return (ft_strdup(""));
-	res = (char *)malloc((end - start + 1) * sizeof(char));
+	res = (char *)malloc((end - start + 1) * sizeof(char *));
 	ft_strlcpy(res, s1 + start, end - start + 2);
 	return (res);
 }
